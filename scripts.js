a4e72f5bd62f943bb5301f7135fb46d7f970d3e4
meteora277 <justin.s.diaz@gmail.com>
2021-10-24 03:35:45 -0400

    created basic markup and styling for bar chart api page

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let barCharts = $(".bar-charts-container")[0]
+
+let graph = {
+
+  X_Axis: "100px",
+  Y_Axis: "100px",
+  background: "black",
+  X_Axis_Steps: 10,
+  Y_Axis_Steps: 10
+
+}
+
+
+
+console.log(barCharts)
