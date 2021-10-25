241da80f6bcc4195ff9d87f8d2c8bf52824ff057
meteora277 <justin.s.diaz@gmail.com>
2021-10-25 13:46:34 -0400

    added create container function, the container everything else will be rendered into

--- scripts.js
+++ scripts.js
@@ -4,18 +4,35 @@ let options = {
   title:"Game Sales 2021",
   titleColor:"#FFFFFF",
   fontSize: "16px",
-  labels:["uwu"],
+  labels:["god of war"],
   labelColor: "#000000",
-  colors: ["#000000","#F0F0F0"],
+  colors: ["#0000FF","#F0F0F0"],
   gap: "30px",
   xAxis: "game",
   yAxis: "units sold",
   yAxisTicks: 10
 };
-let data = [1,2,3,4,9]
+let data = [1,2]
 
+function createContainer(element){
 
-function createBars(array, {colors, gap}){
+  $(element).add(`
+    <div class="title"></div>
+    <div class="bar-charts-container" >
+      <div class="plot-grid y-plot-line x-plot-line">
+        <div class="bar-container">
+
+        </div>
+        <div class="tick-container">
+
+        </div>
+    </div>
+    </div>
+  `).appendTo(element)
+
+}
+
+function createBars(array, { colors, gap, labels }){
 
   let sortedArray = [...array].sort((a,b) => a - b)
   let largestItem = sortedArray[ sortedArray.length -1 ]
@@ -23,8 +40,8 @@ function createBars(array, {colors, gap}){
   array.forEach((item, index) => {
 
     $(".bar-container").add(`
-      <div class="bar" style="height: ${item / largestItem * 100}%; background:${colors[index] || "black"}">
-        uwu
+      <div class="bar" style="height: ${item / largestItem * 100}%; background:${colors[index] || colors[0] || "black"}">
+        <p>${labels[index]|| ""}</p>
       </div`).appendTo(".bar-container")
 
   })
@@ -40,15 +57,15 @@ function createTicks(num){
 
     $(".tick-container").add("<div class='tick'></div>").appendTo(".tick-container")
 
-
   }
 
 }
+let element = document.getElementById("root")
 
+createContainer(element)
 createBars(data, options)
 createTicks(10)
 
-let element = document.getElementById("root")
 
 
 
